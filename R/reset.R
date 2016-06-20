7b75e426b5d149ce3356e3d5d10e3c2c0a513356
Ian K. Kyle <iankyle1@gmail.com>
2016-06-20 13:06:26 -0500

    add some additional features

--- R/reset.R
+++ R/reset.R
@@ -1,6 +1,27 @@
-# function to remove all objects from memory, run garbage collection, and clear the console
-reset <- function(){
-  rm(list=ls(all=TRUE), envir = .GlobalEnv)
-  gc(FALSE)
-  cat('\14')
+# function to remove all objects from memory, run garbage collection, and clear the console. 
+# optionally remove all non-base packages too
+reset <- function(prompt=TRUE, pkgs = FALSE){
+  if (prompt) {
+    p <- readline('Are you sure you want to reset? (y/n): ')
+  } else {
+    p <- 'y'
+  }
+  if (tolower(p) == 'y') {
+    rm(list=ls(.GlobalEnv, all.names=TRUE), 
+       inherits=TRUE)
+    gc(FALSE)
+    cat('\14') 
+    if (pkgs) {
+      pkg <- names(sessionInfo()$otherPkgs)
+      if (!is.null(pkg)) {
+        invisible(sapply(paste0('package:',pkg), 
+                      detach, 
+                      character.only = TRUE, 
+                      unload=TRUE, 
+                      force=TRUE))
+      }
+    }
+  } else {
+    cat('Reset cancelled')
+  }
 }
